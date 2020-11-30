0b8304f79be4ff9fa3a8152ee20923ce5cdde63f
Eristid <eristida.siliute@gmail.com>
2020-11-30 13:59:09 +0200

    js updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-console.log('JS is running...'); 
- import { renderSocials } from "./components/renderSocials.js";
+import { renderSocials } from "./components/renderSocials.js";
+ import { renderSocials } from "./components/socials/renderSocials.js";
  import { socialsData } from './data/socialsData.js';
 
  renderSocials(socialsData); 
